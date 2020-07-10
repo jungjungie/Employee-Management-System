95594de12ccc4adfa640e1f040794c214be6f209
Esther Min <estherjmin@gmail.com>
2020-07-09 22:18:36 -0400

    setup code to add new department data and return erros where applicable

--- ems.js
+++ ems.js
@@ -64,15 +64,13 @@ const updateData = () => {
         switch (updateChoice) {
             case 'Add Employee':
                 addEmployee();
-                mainMenu();
                 break;
             case 'Add Role':
                 console.log('Add Role');
                 mainMenu();
                 break;
             case 'Add Department':
-                console.log('Add Dept');
-                mainMenu();
+                addDept();
                 break;
             case 'Update Employee Role':
                 console.log('Update Employee Role');
@@ -81,6 +79,52 @@ const updateData = () => {
             case 'Go Back':
                 mainMenu();
         }
+    });
+}
+
+// Function to add new department
+const addDept = () => {
+    inquirer.prompt([
+        {
+            message: 'Enter the name of the new department:',
+            name: 'newDept',
+            type: 'input'
+        }
+    ]).then(function ({ newDept }) {
+        connection.query("SELECT department FROM departments", function(err, data) {
+            if (err) throw err;
+
+            // If no departments currently exist, then adds the new department
+            if (data.length === 0) {
+                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function(err, data) {
+                    if(err) throw err;              
+                    console.log(`\n---------------------------------------------------\n`);     
+                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${ newDept }\n`);
+                    console.log(`\n---------------------------------------------------\n`);     
+        
+                    mainMenu();
+                });
+            } else {
+            // Loops through to check for new dept name in database and returns an error if it already exists
+                for (let i=0; i < data.length; i++) {
+                    if (newDept === data[i].department) {
+                        console.log(`\n---------------------------------------------------\n`);     
+                        console.log(`\nERROR: A ${data[i].department} department already exists. Please try again.\n`);
+                        console.log(`\n---------------------------------------------------\n`);     
+                        addDept();
+                        return;
+                    }
+                }
+            // Adds new dept to the database if it does not already exist in database
+                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function(err, data) {
+                    if(err) throw err;
+                    console.log(`\n---------------------------------------------------\n`);     
+                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${ newDept }\n`);
+                    console.log(`\n---------------------------------------------------\n`);     
+                    mainMenu();
+                });
+            }
+        })
     });
 }
 
@@ -127,7 +171,19 @@ const viewDepts = () => {
 const accessSQL = query => {
     connection.query(query, (err, data) => {
         if (err) throw err;
-        console.log('\n\n' + cTable.getTable(data));
-        mainMenu();
+
+        // Returns 'no data exists' if there is no data
+        if (data.length === 0) {
+            console.log(`\n---------------------------------------------------\n`);     
+            console.log('No data exists.')
+            console.log(`\n---------------------------------------------------\n`);   
+            mainMenu();  
+        } else {
+        // Returns available data
+            console.log(`\n---------------------------------------------------\n\n`);     
+            console.log(cTable.getTable(data));
+            console.log(`---------------------------------------------------\n`);     
+            mainMenu();
+        }
     })
 }
