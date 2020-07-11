9b39f35202a4f87e1cc9ca93636591d1090b123a
Esther Min <estherjmin@gmail.com>
2020-07-11 18:56:02 -0400

    Added function to update data for employee role

--- ems.js
+++ ems.js
@@ -16,7 +16,7 @@ let connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    
+
     console.log('================================================\n');
     console.log(figlet.textSync('            EMS', {
         font: 'big',
@@ -74,8 +74,7 @@ const updateData = () => {
                 addDept();
                 break;
             case 'Update Employee Role':
-                console.log('Update Employee Role');
-                mainMenu();
+                updateEmployeeRole();
                 break;
             case 'Go Back':
                 mainMenu();
@@ -85,42 +84,42 @@ const updateData = () => {
 
 // Function to add new employee
 const addEmployee = () => {
-    connection.query("SELECT roles.id AS roleID, roles.position_title AS role, employees.id AS employeeID, CONCAT(employees.first_name, ' ', employees.last_name) AS employee FROM employees RIGHT OUTER JOIN roles ON employees.role_id = roles.id", function(err, data) {
-        if(err) throw err;
+    connection.query("SELECT roles.id AS roleID, roles.position_title AS role, employees.id AS employeeID, CONCAT(employees.first_name, ' ', employees.last_name) AS employee FROM employees RIGHT OUTER JOIN roles ON employees.role_id = roles.id", function (err, data) {
+        if (err) throw err;
         // console.log(data);
 
         let roleChoices = [];
         let mgrChoices = [];
 
-        data.forEach(({roleID, role}, i) => {
+        data.forEach(({ roleID, role }, i) => {
             // Pulls the roleID and role and creates a new object if the values are not null
-            if (roleID && role){
+            if (roleID && role) {
                 const uniqueRole = {
                     name: role,
                     value: roleID
-                }  
+                }
                 //Determines if this is the first occurrence of the role in the array and pushes it to roleChoices if it is
                 const index = data.findIndex(roleObj => roleObj.roleID === roleID);
 
                 if (index === i) {
                     roleChoices.push(uniqueRole);
-                } 
+                }
             }
         })
 
-        data.forEach(({employeeID, employee}, i) => {
+        data.forEach(({ employeeID, employee }, i) => {
             // Pulls the employeeID and employee and creates a new object if the values are not null
-            if (employeeID && employee){
+            if (employeeID && employee) {
                 const uniqueEmployee = {
                     name: employee,
                     value: employeeID
-                }  
+                }
                 //Determines if this is the first occurrence of the role in the array and pushes it to mgrChoices if it is
                 const index = data.findIndex(employeeObj => employeeObj.employeeID === employeeID);
 
                 if (index === i) {
                     mgrChoices.push(uniqueEmployee);
-                } 
+                }
             }
         })
 
@@ -147,15 +146,15 @@ const addEmployee = () => {
                 type: 'list',
                 choices: mgrChoices
             }
-        ]).then(function({ firstName, lastName, role, manager}) {
+        ]).then(function ({ firstName, lastName, role, manager }) {
             console.log(firstName, lastName, role, manager);
 
             // Adds the new employee into database
-            connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [firstName, lastName, role, manager], function(err, data) {
-                if(err) throw err;
-                console.log(`\n---------------------------------------------------\n`);     
-                console.log(`\nNEW EMPLOYEE SUCCESSFULLY ADDED: ${ firstName } ${ lastName }\n`);
-                console.log(`\n---------------------------------------------------\n`);     
+            connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [firstName, lastName, role, manager], function (err, data) {
+                if (err) throw err;
+                console.log(`\n---------------------------------------------------\n`);
+                console.log(`\nNEW EMPLOYEE SUCCESSFULLY ADDED: ${firstName} ${lastName}\n`);
+                console.log(`\n---------------------------------------------------\n`);
                 mainMenu();
             });
         });
@@ -164,13 +163,13 @@ const addEmployee = () => {
 
 // Function to add new role
 const addRole = () => {
-    connection.query("SELECT * FROM departments", function(err, data) {
-        if(err) throw err;
+    connection.query("SELECT * FROM departments", function (err, data) {
+        if (err) throw err;
 
         let deptChoices = [];
         let deptIds = [];
-        
-        for(let i = 0; i < data.length; i++) {
+
+        for (let i = 0; i < data.length; i++) {
             deptChoices.push(data[i].department);
             deptIds.push(data[i].id);
         }
@@ -193,12 +192,12 @@ const addRole = () => {
                 choices: deptChoices
             }
         ]).then(function ({ newRole, salary, dept }) {
-            connection.query("SELECT position_title FROM roles", function(err, data) {
+            connection.query("SELECT position_title FROM roles", function (err, data) {
                 if (err) throw err;
                 let deptId;
 
                 // Selects the dept id of the department selected
-                for (let i=0; i < deptChoices.length; i++) {
+                for (let i = 0; i < deptChoices.length; i++) {
                     if (dept === deptChoices[i]) {
                         deptId = deptIds[i];
                     }
@@ -206,35 +205,35 @@ const addRole = () => {
 
                 // If no roles currently exist, then adds the new role
                 if (data.length === 0) {
-                    connection.query("INSERT INTO roles (position_title, salary, dept_id) VALUES (?, ?, ?)", [newRole, salary, deptId], function(err, data) {
-                        if(err) throw err;              
-                        console.log(`\n---------------------------------------------------\n`);     
-                        console.log(`\nNEW POSITION SUCCESSFULLY ADDED: ${ newRole }\n`);
-                        console.log(`Salary: ${ salary }\n`);
-                        console.log(`Department: ${ dept }\n`);
-                        console.log(`\n---------------------------------------------------\n`);     
-            
+                    connection.query("INSERT INTO roles (position_title, salary, dept_id) VALUES (?, ?, ?)", [newRole, salary, deptId], function (err, data) {
+                        if (err) throw err;
+                        console.log(`\n---------------------------------------------------\n`);
+                        console.log(`\nNEW POSITION SUCCESSFULLY ADDED: ${newRole}\n`);
+                        console.log(`Salary: ${salary}\n`);
+                        console.log(`Department: ${dept}\n`);
+                        console.log(`\n---------------------------------------------------\n`);
+
                         mainMenu();
                     });
                 } else {
-                // Loops through to check for new role in database and returns an error if it already exists
-                    for (let i=0; i < data.length; i++) {
+                    // Loops through to check for new role in database and returns an error if it already exists
+                    for (let i = 0; i < data.length; i++) {
                         if (newRole === data[i].position_title) {
-                            console.log(`\n---------------------------------------------------\n`);     
+                            console.log(`\n---------------------------------------------------\n`);
                             console.log(`\nERROR: The position title ${data[i].position_title} already exists. Please try again.\n`);
-                            console.log(`\n---------------------------------------------------\n`);     
+                            console.log(`\n---------------------------------------------------\n`);
                             addRole();
                             return;
                         }
                     }
-                // Adds new role to the database if it does not already exist in database
-                    connection.query("INSERT INTO roles (position_title, salary, dept_id) VALUES (?, ?, ?)", [newRole, salary, deptId], function(err, data) {
-                        if(err) throw err;
-                        console.log(`\n---------------------------------------------------\n`);     
-                        console.log(`\nNEW POSITION SUCCESSFULLY ADDED: ${ newRole }\n`);
-                        console.log(`Salary: ${ salary }\n`);
-                        console.log(`Department: ${ dept }\n`);
-                        console.log(`\n---------------------------------------------------\n`);     
+                    // Adds new role to the database if it does not already exist in database
+                    connection.query("INSERT INTO roles (position_title, salary, dept_id) VALUES (?, ?, ?)", [newRole, salary, deptId], function (err, data) {
+                        if (err) throw err;
+                        console.log(`\n---------------------------------------------------\n`);
+                        console.log(`\nNEW POSITION SUCCESSFULLY ADDED: ${newRole}\n`);
+                        console.log(`Salary: ${salary}\n`);
+                        console.log(`Department: ${dept}\n`);
+                        console.log(`\n---------------------------------------------------\n`);
                         mainMenu();
                     });
                 }
@@ -252,40 +251,111 @@ const addDept = () => {
             type: 'input'
         }
     ]).then(function ({ newDept }) {
-        connection.query("SELECT department FROM departments", function(err, data) {
+        connection.query("SELECT department FROM departments", function (err, data) {
             if (err) throw err;
 
             // If no departments currently exist, then adds the new department
             if (data.length === 0) {
-                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function(err, data) {
-                    if(err) throw err;              
-                    console.log(`\n---------------------------------------------------\n`);     
-                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${ newDept }\n`);
-                    console.log(`\n---------------------------------------------------\n`);     
-        
+                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function (err, data) {
+                    if (err) throw err;
+                    console.log(`\n---------------------------------------------------\n`);
+                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${newDept}\n`);
+                    console.log(`\n---------------------------------------------------\n`);
+
                     mainMenu();
                 });
             } else {
-            // Loops through to check for new dept name in database and returns an error if it already exists
-                for (let i=0; i < data.length; i++) {
+                // Loops through to check for new dept name in database and returns an error if it already exists
+                for (let i = 0; i < data.length; i++) {
                     if (newDept === data[i].department) {
-                        console.log(`\n---------------------------------------------------\n`);     
+                        console.log(`\n---------------------------------------------------\n`);
                         console.log(`\nERROR: A ${data[i].department} department already exists. Please try again.\n`);
-                        console.log(`\n---------------------------------------------------\n`);     
+                        console.log(`\n---------------------------------------------------\n`);
                         addDept();
                         return;
                     }
                 }
-            // Adds new dept to the database if it does not already exist in database
-                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function(err, data) {
-                    if(err) throw err;
-                    console.log(`\n---------------------------------------------------\n`);     
-                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${ newDept }\n`);
-                    console.log(`\n---------------------------------------------------\n`);     
+                // Adds new dept to the database if it does not already exist in database
+                connection.query("INSERT INTO departments (department) VALUES (?)", [newDept], function (err, data) {
+                    if (err) throw err;
+                    console.log(`\n---------------------------------------------------\n`);
+                    console.log(`\nNEW DEPARTMENT SUCCESSFULLY ADDED: ${newDept}\n`);
+                    console.log(`\n---------------------------------------------------\n`);
                     mainMenu();
                 });
             }
         })
+    });
+}
+
+// Function to update employee's role
+// Update employee's mgr, role, name
+const updateEmployeeRole = () => {
+    connection.query("SELECT roles.id AS roleID, roles.position_title AS role, employees.id AS employeeID, CONCAT(employees.first_name, ' ', employees.last_name) AS employee FROM employees RIGHT OUTER JOIN roles ON employees.role_id = roles.id", function (err, data) {
+        if (err) throw err;
+        // console.log(data);
+
+        let employeeChoices = [];
+        let roleChoices = [];
+
+        data.forEach(({ employeeID, employee }, i) => {
+            // Pulls the employeeID and employee and creates a new object if the values are not null
+            if (employeeID && employee) {
+                const uniqueEmployee = {
+                    name: employee,
+                    value: employeeID
+                }
+                //Determines if this is the first occurrence of the role in the array and pushes it to employeeChoices if it is
+                const index = data.findIndex(employeeObj => employeeObj.employeeID === employeeID);
+
+                if (index === i) {
+                    employeeChoices.push(uniqueEmployee);
+                }
+            }
+        })
+
+        data.forEach(({ roleID, role }, i) => {
+            // Pulls the roleID and role and creates a new object if the values are not null
+            if (roleID && role) {
+                const uniqueRole = {
+                    name: role,
+                    value: roleID
+                }
+                //Determines if this is the first occurrence of the role in the array and pushes it to roleChoices if it is
+                const index = data.findIndex(roleObj => roleObj.roleID === roleID);
+
+                if (index === i) {
+                    roleChoices.push(uniqueRole);
+                }
+            }
+        })
+
+        inquirer.prompt([
+            {
+                message: 'Which employee\'s role do you want to update?',
+                name: 'employee',
+                type: 'list',
+                choices: employeeChoices
+            },
+            {
+                message: 'What is the employee\'s new role?',
+                name: 'newRole',
+                type: 'list',
+                choices: roleChoices
+            }
+        ]).then(function ({ employee, newRole }) {
+            // Updates the employee role in database
+            connection.query("UPDATE employees SET ? WHERE ?", [
+                { role_id: newRole },
+                { id: employee }
+            ], function (err, data) {
+                if (err) throw err;
+                console.log(`\n---------------------------------------------------\n`);
+                console.log(`\nEMPLOYEE DATA SUCCESSFULLY UPDATED\n`);
+                console.log(`\n---------------------------------------------------\n`);
+                mainMenu();
+            });
+        });
     });
 }
 
@@ -338,15 +408,15 @@ const accessSQL = query => {
 
         // Returns 'no data exists' if there is no data
         if (data.length === 0) {
-            console.log(`\n---------------------------------------------------\n`);     
+            console.log(`\n---------------------------------------------------\n`);
             console.log('No data exists.')
-            console.log(`\n---------------------------------------------------\n`);   
-            mainMenu();  
+            console.log(`\n---------------------------------------------------\n`);
+            mainMenu();
         } else {
-        // Returns available data
-            console.log(`\n---------------------------------------------------\n\n`);     
+            // Returns available data
+            console.log(`\n---------------------------------------------------\n\n`);
             console.log(cTable.getTable(data));
-            console.log(`---------------------------------------------------\n`);     
+            console.log(`---------------------------------------------------\n`);
             mainMenu();
         }
     })
